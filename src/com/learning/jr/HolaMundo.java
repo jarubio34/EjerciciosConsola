88810f08184d032ee6874cbe0e9b2d6c70c282f6
Jean <jarubio34@outlook.com>
2019-02-03 01:19:58 -0500

    Modificacion primera clase

--- src/com/learning/jr/HolaMundo.java
+++ src/com/learning/jr/HolaMundo.java
@@ -2,10 +2,10 @@ package com.learning.jr;
 
 public class HolaMundo {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo from Eclipse!!");
 
-	}
+	}*/
 
 }
